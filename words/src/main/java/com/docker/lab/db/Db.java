7466830f26c9b60af56940b3d96b70132f64f9ac
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2016-04-08 16:14:54 +0200

    Better starting examples
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- words/src/main/java/com/docker/lab/db/Db.java
+++ words/src/main/java/com/docker/lab/db/Db.java
@@ -5,13 +5,13 @@ public class Db {
     public void init() {
         AllAdjectives allAdjectives = new AllAdjectives();
         allAdjectives.reset();
-        allAdjectives.addAll("exquis", "grand", "petit", "rouge", "flottant");
+        allAdjectives.addAll("exquis", "rose", "un peu pourri", "rouge", "flottant", "le programmeur");
         AllNouns allNouns = new AllNouns();
         allNouns.reset();
-        allNouns.addAll("le cadavre", "l'éléphant", "la rose", "le tuba");
+        allNouns.addAll("le cadavre", "l'éléphant", "le langage scala", "le laptop");
         AllVerbs allVerbs = new AllVerbs();
         allVerbs.reset();
-        allVerbs.addAll("boiras", "s'eteins", "rougit", "s'endors", "marche");
+        allVerbs.addAll("boiras", "explose", "fait fumer", "endors", "marche vers");
     }
 
     public static void main(String[] args) {
