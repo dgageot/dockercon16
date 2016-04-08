0ea16819095cb582b14c5e3b9b626f023f26d894
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2016-04-08 16:18:16 +0200

    more words
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- words/src/main/java/com/docker/lab/db/Db.java
+++ words/src/main/java/com/docker/lab/db/Db.java
@@ -5,7 +5,7 @@ public class Db {
     public void init() {
         AllAdjectives allAdjectives = new AllAdjectives();
         allAdjectives.reset();
-        allAdjectives.addAll("exquis", "rose", "un peu pourri", "rouge", "flottant", "le programmeur");
+        allAdjectives.addAll("exquis", "rose", "un peu pourri", "rouge", "flottant", "du programmeur");
         AllNouns allNouns = new AllNouns();
         allNouns.reset();
         allNouns.addAll("le cadavre", "l'éléphant", "le langage scala", "le laptop");
