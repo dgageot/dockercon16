b2a01766520f1e3fe89fd79dca196f508f2c89b8
David Gageot <david@gageot.net>
2016-04-09 22:57:53 +0200

    Simpler push script
    Signed-off-by: David Gageot <david@gageot.net>

--- push.sh
+++ push.sh
@@ -2,17 +2,11 @@
 
 set -e
 
-pushd web
-docker build -t dgageot/lab-web .
+docker build -t dgageot/lab-web web
 docker push dgageot/lab-web
-popd
 
-pushd words-dispatcher/
-docker build -t dgageot/lab-words-dispatcher .
+docker build -t dgageot/lab-words-dispatcher words-dispatcher
 docker push dgageot/lab-words-dispatcher
-popd
 
-pushd words-java
-docker build -t dgageot/lab-words-java .
+docker build -t dgageot/lab-words-java words-java
 docker push dgageot/lab-words-java
-popd
