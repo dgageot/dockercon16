f6628ceb5624cf22793087d9c63689243fbb5fd2
David Gageot <david@gageot.net>
2016-04-14 13:43:14 +0200

    Use dockerdemos hub account

--- push.sh
+++ push.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-docker build -t dgageot/lab-web web
-docker push dgageot/lab-web
+docker build -t dockerdemos/lab-web web
+docker push dockerdemos/lab-web
 
-docker build -t dgageot/lab-words-dispatcher words-dispatcher
-docker push dgageot/lab-words-dispatcher
+docker build -t dockerdemos/lab-words-dispatcher words-dispatcher
+docker push dockerdemos/lab-words-dispatcher
 
-docker build -t dgageot/lab-words-java words-java
-docker push dgageot/lab-words-java
+docker build -t dockerdemos/lab-words-java words-java
+docker push dockerdemos/lab-words-java
