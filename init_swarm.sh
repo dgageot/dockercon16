88850af1d83ef749d62cb37c307f4c28c7a95c28
David Gageot <david@gageot.net>
2016-04-14 13:41:22 +0200

    Create on google cloud
    Signed-off-by: David Gageot <david@gageot.net>

--- init_swarm.sh
+++ init_swarm.sh
@@ -2,27 +2,45 @@
 
 set -e
 
-docker-machine create -d virtualbox kv
+docker-machine create -d google kv
 
 docker $(docker-machine config kv) run -d -p "8500:8500" --name="consul" --restart "always" -h "consul" progrium/consul -server -bootstrap
 
-docker-machine create -d virtualbox --swarm --swarm-master \
+docker-machine create -d google --swarm --swarm-master \
     --swarm-discovery="consul://$(docker-machine ip kv):8500" \
     --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth1:2376" \
+    --engine-opt="cluster-advertise=eth0:2376" \
     master
 
-docker-machine create -d virtualbox --swarm \
+docker-machine create -d google --swarm \
     --swarm-discovery="consul://$(docker-machine ip kv):8500" \
     --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth1:2376" \
+    --engine-opt="cluster-advertise=eth0:2376" \
     node-01
 
-docker-machine create -d virtualbox --swarm \
+docker-machine create -d google --swarm \
     --swarm-discovery="consul://$(docker-machine ip kv):8500" \
     --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth1:2376" \
+    --engine-opt="cluster-advertise=eth0:2376" \
     node-02
+
+docker-machine create -d google --swarm \
+    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
+    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
+    --engine-opt="cluster-advertise=eth0:2376" \
+    node-03
+
+docker-machine create -d google --swarm \
+    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
+    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
+    --engine-opt="cluster-advertise=eth0:2376" \
+    node-04
+
+docker-machine create -d google --swarm \
+    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
+    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
+    --engine-opt="cluster-advertise=eth0:2376" \
+    node-05
 
 eval $(docker-machine env --swarm master)
 docker network create --driver overlay lab-net
