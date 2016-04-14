58490a27cd78538d5bfbf855159ac828ebdb3dbd
David Gageot <david@gageot.net>
2016-04-14 15:11:31 +0200

    Format code and proper error management
    Signed-off-by: David Gageot <david@gageot.net>

--- words-dispatcher/dispatcher.go
+++ words-dispatcher/dispatcher.go
@@ -1,38 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
-  "fmt"
+	"math/rand"
 	"net"
 	"net/http"
-  "math/rand"
 )
 
 func main() {
-  rand.Seed(42);
+	rand.Seed(42)
 	http.HandleFunc("/", forward("words-java", 8080))
 	http.ListenAndServe(":8080", nil)
 }
 
 func forward(host string, port int) func(w http.ResponseWriter, r *http.Request) {
-  
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.RequestURI)
-   
+
 		addrs, err := net.LookupHost(host)
 		if err != nil {
-			log.Println(err)
-		} else {
-			log.Println("all available ips", addrs)
+			log.Println("Error", err)
+			http.Error(w, err.Error(), 500)
+			return
 		}
-    
-    ip := addrs[rand.Intn(len(addrs))]
-    log.Println("Choosen ip", ip)
-    
-    url := fmt.Sprintf("http://%s:%d%s", ip, port, r.RequestURI)
-    
-    log.Println("Asking to", url)
+
+		log.Println("all available ips", addrs)
+		ip := addrs[rand.Intn(len(addrs))]
+		log.Println("Choosen ip", ip)
+
+		url := fmt.Sprintf("http://%s:%d%s", ip, port, r.RequestURI)
+		log.Println("Asking to", url)
 
 		resp, err := http.Get(url)
 		if err != nil {
