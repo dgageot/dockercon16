a01c1015463fb3c04ae98115327bd27e36305888
David Gageot <david@gageot.net>
2016-04-14 10:14:09 +0200

    Dispatcher choose a random ip
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- words-dispatcher/dispatcher.go
+++ words-dispatcher/dispatcher.go
@@ -3,32 +3,42 @@ package main
 import (
 	"io/ioutil"
 	"log"
+  "fmt"
 	"net"
 	"net/http"
+  "math/rand"
 )
 
 func main() {
-	http.HandleFunc("/", forward("http://words-java:8080"))
+  rand.Seed(42);
+	http.HandleFunc("/", forward("words-java", 8080))
 	http.ListenAndServe(":8080", nil)
 }
 
-func forward(url string) func(w http.ResponseWriter, r *http.Request) {
+func forward(host string, port int) func(w http.ResponseWriter, r *http.Request) {
+  
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.RequestURI)
+   
+		addrs, err := net.LookupHost(host)
+		if err != nil {
+			log.Println(err)
+		} else {
+			log.Println("all available ips", addrs)
+		}
+    
+    ip := addrs[rand.Intn(len(addrs))]
+    log.Println("Choosen ip", ip)
+    
+    url := fmt.Sprintf("http://%s:%d%s", ip, port, r.RequestURI)
+    
+    log.Println("Asking to", url)
 
-		resp, err := http.Get(url + r.RequestURI)
+		resp, err := http.Get(url)
 		if err != nil {
 			log.Println("Error", err)
 			http.Error(w, err.Error(), 500)
 			return
-		}
-
-		log.Println("answered by", r.RemoteAddr)
-		addrs, err := net.LookupHost("words-java")
-		if err != nil {
-			log.Println(err)
-		} else {
-			log.Println(addrs)
 		}
 
 		buf, err := ioutil.ReadAll(resp.Body)
