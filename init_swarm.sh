7966b6c66dcd4aab3e0609488dce9036ec51a235
David Gageot <david@gageot.net>
2016-04-16 10:17:12 +0200

    Remove duplication in create script
    Signed-off-by: David Gageot <david@gageot.net>

--- init_swarm.sh
+++ init_swarm.sh
@@ -4,7 +4,10 @@ set -e
 
 docker-machine create -d google kv
 
-docker $(docker-machine config kv) run -d -p "8500:8500" --name="consul" --restart "always" -h "consul" progrium/consul -server -bootstrap
+docker $(docker-machine config kv) run \
+    -d -p "8500:8500" \
+    --name="consul" --restart "always" -h "consul" \
+    progrium/consul -server -bootstrap
 
 docker-machine create -d google --swarm --swarm-master \
     --swarm-discovery="consul://$(docker-machine ip kv):8500" \
@@ -12,35 +15,14 @@ docker-machine create -d google --swarm --swarm-master \
     --engine-opt="cluster-advertise=eth0:2376" \
     master
 
-docker-machine create -d google --swarm \
-    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth0:2376" \
-    node-01
+for i in {1..5}; do
+  name=$(printf "%0.2d" $i)
 
-docker-machine create -d google --swarm \
-    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth0:2376" \
-    node-02
+  docker-machine create -d google --swarm \
+      --swarm-discovery="consul://$(docker-machine ip kv):8500" \
+      --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
+      --engine-opt="cluster-advertise=eth0:2376" \
+      $name
+done
 
-docker-machine create -d google --swarm \
-    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth0:2376" \
-    node-03
-
-docker-machine create -d google --swarm \
-    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth0:2376" \
-    node-04
-
-docker-machine create -d google --swarm \
-    --swarm-discovery="consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
-    --engine-opt="cluster-advertise=eth0:2376" \
-    node-05
-
-eval $(docker-machine env --swarm master)
-docker network create --driver overlay lab-net
+docker $(docker-machine config --swarm master) network create --driver overlay lab-net
