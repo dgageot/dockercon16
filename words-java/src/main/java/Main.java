bf6b3504a69107d23a12ac5d574a4451f7f0afd7
David Gageot <david@gageot.net>
2016-04-21 19:18:29 +0200

    Add hostname
    Signed-off-by: David Gageot <david@gageot.net>

--- words-java/src/main/java/Main.java
+++ words-java/src/main/java/Main.java
@@ -4,13 +4,15 @@ import net.codestory.http.WebServer;
 import net.codestory.http.filters.log.*;
 import org.jongo.Jongo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Random;
 
 public class Main {
     private static final Random random = new Random();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         // see https://github.com/bguerout/jongo/issues/289
         DB db = new MongoClient("mongo:27017").getDB("lab-docker");
         Jongo jongo = new Jongo(db);
@@ -23,15 +25,31 @@ public class Main {
         nouns.addIfEmpty("le cadavre", "l'éléphant", "le langage scala", "le laptop");
         adjectives.addIfEmpty("exquis", "rose", "un peu pourri", "rouge", "flottant", "du programmeur");
 
+        String hostname = InetAddress.getLocalHost().getHostAddress();
+
         new WebServer().configure(routes -> routes
                 .filter(LogRequestFilter.class)
-                .get("/verb", () -> random(verbs.all()))
-                .get("/adjective", () -> random(adjectives.all()))
-                .get("/noun", () -> random(nouns.all()))
+                .get("/verb", () -> toResponse(hostname, random(verbs.all())))
+                .get("/adjective", () -> toResponse(hostname, random(adjectives.all())))
+                .get("/noun", () -> toResponse(hostname, random(nouns.all())))
         ).start();
+    }
+
+    private static WordResponse toResponse(String hostname, String word) {
+        return new WordResponse(word, hostname);
     }
 
     private static String random(List<String> values) {
         return values.get(random.nextInt(values.size()));
+    }
+
+    public static class WordResponse {
+        String word;
+        String hostname;
+
+        WordResponse(String word, String hostname) {
+            this.word = word;
+            this.hostname = hostname;
+        }
     }
 }
