25c8a5074f041a100adb23cf7a936668dd071d08
David Gageot <david@gageot.net>
2016-06-01 18:56:01 +0200

    Push images to a tarball
    Signed-off-by: David Gageot <david@gageot.net>

--- push.sh
+++ push.sh
@@ -8,3 +8,6 @@ for folder in $folders; do
   docker build -t dockerdemos/lab-$folder $folder
   docker push dockerdemos/lab-$folder
 done
+
+docker save -o images.tar dockerdemos/lab-web dockerdemos/lab-words-dispatcher dockerdemos/lab-words-java mongo-express:0.30.43 mongo:3.2.4
+#docker load -i images.tar
