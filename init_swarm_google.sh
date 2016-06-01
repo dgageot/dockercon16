41a47449139276be9b98e9e96fd57f5aa44d8853
David Gageot <david@gageot.net>
2016-06-01 20:14:28 +0200

    Fix swarm script
    Signed-off-by: David Gageot <david@gageot.net>

--- init_swarm_google.sh
+++ init_swarm_google.sh
@@ -2,23 +2,32 @@
 
 set -e
 
-docker-machine create -d google kv
+export GOOGLE_PROJECT="code-story-blog"
+export GOOGLE_ZONE="europe-west1-d"
+export GOOGLE_DISK_SIZE="1000"
+export GOOGLE_MACHINE_TYPE="custom-2-8192"
+
+docker-machine create -d google --google-address ip-kv kv
 
 docker $(docker-machine config kv) run \
     -d -p "8500:8500" \
     --name="consul" --restart "always" -h "consul" \
     progrium/consul -server -bootstrap
 
-docker-machine create -d google --swarm --swarm-master \
+docker-machine create -d google \
+    --google-address ip-master \
+    --swarm --swarm-master \
     --swarm-discovery="consul://$(docker-machine ip kv):8500" \
     --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
     --engine-opt="cluster-advertise=eth0:2376" \
     master
 
-for i in {1..5}; do
+for i in {1..4}; do
   name=$(printf "node-%0.2d" $i)
+  ip=$(printf "ip-node%0.2d" $i)
 
-  docker-machine create -d google --swarm \
+  docker-machine -D create -d google --swarm \
+      --google-address $ip \
       --swarm-discovery="consul://$(docker-machine ip kv):8500" \
       --engine-opt="cluster-store=consul://$(docker-machine ip kv):8500" \
       --engine-opt="cluster-advertise=eth0:2376" \
