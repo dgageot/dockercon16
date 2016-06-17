1cd96ab7881e3817fa6cdc1466b64a2c8ef6019a
David Gageot <david@gageot.net>
2016-06-17 14:11:15 -0700

    DockerCon scale demo
    Signed-off-by: David Gageot <david@gageot.net>

--- words-java/src/main/java/Main.java
+++ words-java/src/main/java/Main.java
@@ -13,35 +13,42 @@ public class Main {
     private static final Random random = new Random();
 
     public static void main(String[] args) throws UnknownHostException {
-        // see https://github.com/bguerout/jongo/issues/289
-        DB db = new MongoClient("mongo:27017").getDB("lab-docker");
-        Jongo jongo = new Jongo(db);
-
-        Words nouns = new Words(jongo, "nouns");
-        Words verbs = new Words(jongo, "verbs");
-        Words adjectives = new Words(jongo, "adjectives");
-
-        nouns.addIfEmpty("dead body", "elephant", "go language", "laptop", "container", "micro-service");
-        verbs.addIfEmpty("will drink", "smashes", "smokes", "eats", "walks towards", "loves", "helps");
-        adjectives.addIfEmpty("the exquisite", "a pink", "the rotten", "a red", "the floating", "a broken", "a shiny", "the pretty");
-
-        // String hostname = InetAddress.getLocalHost().getHostAddress();
         String hostname = InetAddress.getLocalHost().getHostName();
+
+        WordResponse verb = new WordResponse(randomWord("verbs"), hostname);
+        WordResponse adjective = new WordResponse(randomWord("adjectives"), hostname);
+        WordResponse noun = new WordResponse(randomWord("nouns"), hostname);
 
         new WebServer().configure(routes -> routes
                 .filter(LogRequestFilter.class)
-                .get("/verb", () -> toResponse(hostname, random(verbs.all())))
-                .get("/adjective", () -> toResponse(hostname, random(adjectives.all())))
-                .get("/noun", () -> toResponse(hostname, random(nouns.all())))
+                .get("/verb", verb)
+                .get("/adjective", adjective)
+                .get("/noun", noun)
         ).start();
     }
 
-    private static WordResponse toResponse(String hostname, String word) {
-        return new WordResponse(word, hostname);
-    }
+    private static String randomWord(String set) {
+        // see https://github.com/bguerout/jongo/issues/289
+        DB db = new MongoClient("mongo:27017").getDB("lab-docker");
 
-    private static String random(List<String> values) {
-        return values.get(random.nextInt(values.size()));
+        Jongo jongo = new Jongo(db);
+        Words words = new Words(jongo, set);
+
+        switch (set) {
+            case "nouns":
+                words.addIfEmpty("dead body", "elephant", "go language", "laptop", "container", "micro-service");
+                break;
+            case "verbs":
+                words.addIfEmpty("will drink", "smashes", "smokes", "eats", "walks towards", "loves", "helps");
+                break;
+            case "adjectives":
+                words.addIfEmpty("the exquisite", "a pink", "the rotten", "a red", "the floating", "a broken", "a shiny", "the pretty");
+                break;
+        }
+
+        Random random = new Random();
+        List<String> all = words.all();
+        return all.get(random.nextInt(all.size()));
     }
 
     public static class WordResponse {
