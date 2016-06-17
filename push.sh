7536f3c5c5c325b996cb5915c5f13a79ed67b924
David Gageot <david@gageot.net>
2016-06-17 11:55:25 -0700

    Use compose
    Signed-off-by: David Gageot <david@gageot.net>

--- push.sh
+++ push.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-folders="web words-dispatcher words-java"
+cp docker-compose-v1.yml docker-compose.yml
 
-for folder in $folders; do
-  docker build -t dockerdemos/lab-$folder $folder
-  docker push dockerdemos/lab-$folder
-done
-
-docker pull mongo:3.2.4
+docker-compose build
+docker-compose push
+docker-compose pull db
 
 docker save -o images.tar dockerdemos/lab-web dockerdemos/lab-words-dispatcher dockerdemos/lab-words-java mongo:3.2.4
