8b1a6042f89f7acbd1f282ce039b1aaa694263b0
David Gageot <david@gageot.net>
2016-06-18 17:46:09 -0700

    fix push script
    Signed-off-by: David Gageot <david@gageot.net>

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ set -e
 
 docker-compose build
 docker-compose push
-docker-compose pull db
+docker-compose pull mongo
 
 docker save -o images.tar dockerdemos/lab-web dockerdemos/lab-words-dispatcher dockerdemos/lab-words-java mongo:3.2.4
